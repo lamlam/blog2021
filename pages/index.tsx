6cc5a414ea3455d0257c2f9514a3de12a51b7842
lamlam <lamlam4r@gmail.com>
2021-01-02 16:04:19 +0900

    Set up chakra-ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,15 @@
 import Head from "next/head";
+import { Box, Heading } from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <>
+    <Box p={4}>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Hello World</h1>
-    </>
+      <Heading>Hello World</Heading>
+    </Box>
   );
 }
