32854486e845b542f75c27fc8706942202689480
lamlam <lamlam4r@gmail.com>
2021-01-21 09:03:43 +0900

    Refactor ArticleHeader

--- components/ArticleHeader.tsx
+++ components/ArticleHeader.tsx
@@ -6,19 +6,17 @@ type Props = {
   date?: Date;
 };
 
-export default function Title(props: Props) {
+export default function Title({ title, date }: Props) {
   return (
     <Container centerContent p="40px 0" maxWidth="100%">
       <Head>
-        <title>{props.title}</title>
+        <title>{title}</title>
         <link rel="icon" href="/icon.svg" type="image/svg+xml" />
       </Head>
-      <Heading>{props.title}</Heading>
-      {props.date ? (
-        <Text color="gray.500">
-          {props.date.toISOString().split("T").shift()}
-        </Text>
-      ) : null}
+      <Heading paddingBottom="10px">{title}</Heading>
+      {date && (
+        <Text color="gray.500">{date.toISOString().split("T").shift()}</Text>
+      )}
     </Container>
   );
 }
