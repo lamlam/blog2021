1a617b77de9f1c3cdb4f39ba36f33f9dbc95014b
lamlam <lamlam4r@gmail.com>
2021-01-31 16:38:53 +0900

    Delete test posts

--- model/Posts.ts
+++ model/Posts.ts
@@ -5,11 +5,4 @@ import { post as nextjsChakrauiOGP } from "../pages/posts/nextjs-chakraui-ogp";
 
 export const Posts: { [key: string]: Post } = {
   [nextjsChakrauiOGP.id]: nextjsChakrauiOGP,
-  [nextjsChakraui.id]: nextjsChakraui,
-  test: {
-    title: "tests",
-    summary: "yattaze test is test is test is jjjjjjjjl where",
-    id: "blogii",
-    createdDate: "2021-01-06",
-  },
 };
