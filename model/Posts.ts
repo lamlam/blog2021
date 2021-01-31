a470804ae73cb55da9d0a8a3c39e62365ba24410
lamlam <lamlam4r@gmail.com>
2021-01-31 16:47:40 +0900

    Remove unused code

--- model/Posts.ts
+++ model/Posts.ts
@@ -1,6 +1,5 @@
 import { Post } from "../interface/Post";
 
-import { post as nextjsChakraui } from "../pages/posts/blog-nextjs-chakraui";
 import { post as nextjsChakrauiOGP } from "../pages/posts/nextjs-chakraui-ogp";
 
 export const Posts: { [key: string]: Post } = {
